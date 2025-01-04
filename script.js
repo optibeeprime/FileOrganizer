d715f17a1ea99c6b0eab13e736c289bf571e49ba
optibeeprime <105331080+optibeeprime@users.noreply.github.com>
2025-01-04 23:09:23 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.querySelectorAll('.slide');
+    slides.forEach(slide => slide.style.display = 'none');
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = 'block';
+    setTimeout(showSlides, 3000); // 3秒ごとに次の画像へ
+}
