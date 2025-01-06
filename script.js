12e7e6ff09ca0b44a78da7520d493ee1560fcce2
optibeeprime <105331080+optibeeprime@users.noreply.github.com>
2025-01-07 01:11:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ showSlides();
 
 function showSlides() {
     let slides = document.querySelectorAll('.slide');
-    slides.forEach((slide, index) => {
-        slide.style.display = 'none';
-        slide.style.transform = 'translateX(0)';
-    });
+    slides.forEach(slide => slide.style.display = 'none');
 
     slideIndex++;
     if (slideIndex > slides.length) {
@@ -14,25 +11,4 @@ function showSlides() {
     }
     slides[slideIndex - 1].style.display = 'block';
     setTimeout(showSlides, 3000); // 3秒ごとに次の画像へ
-}
-function plusSlides(n) {
-    let slides = document.querySelectorAll('.slide');
-    slides.forEach((slide, index) => {
-        slide.style.display = 'none';
-        slide.style.transform = 'translateX(0)';
-    });
-
-    slideIndex += n;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    if (slideIndex < 1) {
-        slideIndex = slides.length;
-    }
-
-    slides[slideIndex - 1].style.display = 'block';
-    slides[slideIndex - 1].style.transform = n > 0 ? 'translateX(100%)' : 'translateX(-100%)';
-    setTimeout(() => {
-        slides[slideIndex - 1].style.transform = 'translateX(0)';
-    }, 1); // アニメーション
 }
