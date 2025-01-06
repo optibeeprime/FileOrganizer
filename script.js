2c99bb2104f51e6d714bd37d2c9e1f0e040278fc
optibeeprime <105331080+optibeeprime@users.noreply.github.com>
2025-01-07 02:09:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ function showSlides() {
     slides[slideIndex - 1].style.display = 'block';
     setTimeout(showSlides, 3000); // 3秒ごとに次の画像へ
 }
+function plusSlides(n) {
+    showSlides(slideIndex += n, true);
+}
+
+function showSlides(n, userTriggered = false) {
+    let slides = document.querySelectorAll('.slide');
+    if (n === undefined) { 
+        slideIndex++;
+    } else {
+        slideIndex = n;
+    }
+
+    if (slideIndex >= slides.length) { 
+        slideIndex = 0 
+    }
+    if (slideIndex < 0) { 
+        slideIndex = slides.length - 1 
+    }
+
+    slides.forEach(slide => slide.style.display = 'none');
+    slides[slideIndex].style.display = 'block';
+
+    if (!userTriggered) {
+        setTimeout(() => showSlides(undefined, false), 3000); // 3秒ごとに次の画像へ
+    }
+}
